06e0832c5e9248c18bfab6ce65fc403bcd7827ab
Sakura <m.aguiar0954@gmail.com>
2022-03-09 12:38:50 -0500

    Added "remaining letters" and "used letters"

--- main.py
+++ main.py
@@ -4,7 +4,11 @@
 #################
 
 #-- Imports
-import random
+import random, os
+
+#-- Alphabet list
+alpha = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+popped = []
 
 #-- Functions
 def get_word():
@@ -25,16 +29,31 @@ def main():
     wordle = list(word)
     output = list(wordLen)
     while True:
-        character = input("Input a single character: ")
+        character = input("Input a single letter: ")
         count = 0
         for x in wordle:
             if character == x:
                 output[count] = character
             count += 1
-        print("".join(output))
         if output == wordle:
             break
-    print("Congrats!")
+        else:
+            os.system("cls")
+            check = False
+            for x in  range(len(alpha)):
+                if character == alpha[x]:
+                    pop = alpha.pop(x)
+                    popped.append(pop)
+                    check = True
+                    break
+            if not(check):
+                for x in range(len(popped)):
+                    if character == popped[x]:
+                        print("You have already tried that! \n")
+            print("Remaining letters: " + str(alpha) + "\n")
+            print("".join(output))
+    os.system("cls")
+    print("Congrats! The word is: " + word)
     return 0
 
 if __name__ == "__main__":
